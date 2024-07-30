461b41a91f9cbacd7eacffb93544fab15ccdfe75
rizky <orangsuksesrizky@gmail.com>
2024-07-29 20:04:10 -1200

    api hari ke 2

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -74,4 +74,61 @@ class KategoriController extends Controller
 
         }
     }
+
+    
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_kategori' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $res = [
+                'success' => false,
+                'message' => 'Validasi Gagal',
+                'errors' => $validator->errors(),
+            ];
+            return response()->json($res, 422);
+        }
+
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $kategori->nama_kategori = $request->nama_kategori;
+            $kategori->slug = Str::slug($request->nama_kategori);
+            $kategori->save();
+            $res = [
+                'success' => true,
+                'message' => 'Data Kategori Tersimpan',
+                'data' => $kategori,
+            ];
+            return response()->json($res, 200);
+        } catch (\Exception $e) {
+            $res = [
+                'success' => false,
+                'message' => 'Terjadi kesalahan',
+                'errors' => $e->getMessage(),
+            ];
+            return response()->json($res, 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $delete->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'kategori' . $kategori->nama_kategori. 'berhasil dihapus',
+                'data' => $kategori,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'detail kategori',
+                'errors' => $e->getMessage(),
+            ], 404);
+
+        }
+    }
+
 }

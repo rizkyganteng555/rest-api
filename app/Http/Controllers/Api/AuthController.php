cbf503adea62d282600c524e13a5638c2d1aa0e5
rizky <orangsuksesrizky@gmail.com>
2024-08-01 18:44:09 -1200

    nn

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
     }
     public function logout()
     {
-        Auth::user()->tokens()->delete();
+        Auth::user()->tokens->delete();
         $res = [
             'Message' => 'log out berhasil'
         ];
